7165ea07f28235f4da432f2fe404808100e8f4a3
akshay ranga <akshauranga9478@gmail.com>
2017-07-08 18:42:28 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@ use REACHING_HANDS;
 
 create table IF NOT EXISTS INVENTORY
 	(
-		item_id varchar(15) primary key,
+		item_id int primary key AUTO_INCREMENT,
 		item_name varchar(25) not null,
 		current_count int,
 		threshold_count int,
