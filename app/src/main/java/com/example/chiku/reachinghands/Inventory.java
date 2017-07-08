08f134ec8a2b3d878b3604bf76a01834e5d8029e
Ashish <ash.95cool@gmail.com>
2017-07-08 18:26:31 +0530

    inventory1

--- app/src/main/java/com/example/chiku/reachinghands/Inventory.java
+++ app/src/main/java/com/example/chiku/reachinghands/Inventory.java
@@ -1,13 +1,20 @@
 package com.example.chiku.reachinghands;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 public class Inventory extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment frag = fm.findFragmentById(R.id.inventory_container);
+        if(frag == null){
+            frag = new InventoryList();
+            fm.beginTransaction().add(R.id.inventory_container, frag).commit();
+        }
     }
 }
--- app/src/main/java/com/example/chiku/reachinghands/InventoryList.java
+++ app/src/main/java/com/example/chiku/reachinghands/InventoryList.java
@@ -1,108 +1,79 @@
 package com.example.chiku.reachinghands;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link InventoryList.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link InventoryList#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class InventoryList extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public InventoryList() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InventoryList.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static InventoryList newInstance(String param1, String param2) {
-        InventoryList fragment = new InventoryList();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    RecyclerView recyclerView;
+    InventoryAdaptor inventoryAdaptor;
+    List<InventorySingle> inventoryList = new ArrayList<>();
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_inventory_list, container, false);
+
+        //new inventories
+        inventoryList.add(new InventorySingle("1","clothes", 25));
+        inventoryList.add(new InventorySingle("2","clothes", 25));
+        inventoryList.add(new InventorySingle("3","vegetables", 35));
+        inventoryList.add(new InventorySingle("4","bottles", 30));
+
+        recyclerView = (RecyclerView)view.findViewById(R.id.inventory_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        inventoryAdaptor = new InventoryAdaptor();
+        recyclerView.setAdapter(inventoryAdaptor);
+        //inventoryAdaptor.notifyDataSetChanged();
+        return view;
+    }
+
+
+    private class InventoryHolder extends RecyclerView.ViewHolder{
+        private TextView item_name;
+        private TextView current_item_count;
+
+        public InventoryHolder(View itemView) {
+            super(itemView);
+            item_name = (TextView)itemView.findViewById(R.id.item_name);
+            current_item_count = (TextView)itemView.findViewById(R.id.current_item_count);
+        }
+        public void bindInventory(InventorySingle inventorySingle){
+            item_name.setText(inventorySingle.getName());
+            current_item_count.setText(inventorySingle.getCurrent_count()+"");
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inventory_list, container, false);
-    }
+    private class InventoryAdaptor extends RecyclerView.Adapter<InventoryHolder>{
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+        @Override
+        public InventoryHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            View view = inflater.inflate(R.layout.activity_inventory_holder, parent, false);
+            return new InventoryHolder(view);
         }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+        @Override
+        public void onBindViewHolder(InventoryHolder holder, int position) {
+            InventorySingle inventorySingle = inventoryList.get(position);
+            Toast.makeText(getActivity(), inventorySingle.getName(), Toast.LENGTH_SHORT).show();
+            holder.bindInventory(inventorySingle);
         }
-    }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        @Override
+        public int getItemCount() {
+            return inventoryList.size();
+        }
     }
 }
