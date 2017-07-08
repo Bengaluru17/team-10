22a76466020b48dbb338d5cab72a046dfb2b34ab
Vaibhav <vaibhav.1si14cs136@gmail.com>
2017-07-08 18:10:42 +0530

    Splash Commited

--- app/src/main/java/com/example/chiku/reachinghands/AfterLogin.java
+++ app/src/main/java/com/example/chiku/reachinghands/AfterLogin.java
@@ -1,6 +1,5 @@
 package com.example.chiku.reachinghands;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,17 +11,16 @@ public class AfterLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_login);
     }
-    public void btn_approvalreq(View v){
+    public void BtnInventory(View view)
+    {
 
-        Intent i = new Intent(this,RequestsForApproval.class);
-        startActivity(i);
     }
-    public void btn_Inventory(View v){
-        Intent i = new Intent(this,Inventory.class);
-        startActivity(i);
+    public void BtnApprovedReq(View view)
+    {
+
     }
-    public void btn_approvedreq(View v){
-        Intent i = new Intent(this,ApprovedReq.class);
-        startActivity(i);
+    public void BtnReqForApproval(View view)
+    {
+
     }
 }
