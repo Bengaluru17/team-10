faee1a99c56d12ff29eee6eb5db0230ed0ffe979
Kenil Patel <kenspatel96@gmail.com>
2017-07-08 15:33:59 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,6 +1,6 @@
 create database if not exist REACHING_HANDS;
 use REACHING_HANDS;
-create table if not exist INVENTORY
+create table INVENTORY
 	(
 		item_id varchar(15) primary key,
 		item_name varchar(25) not null,
